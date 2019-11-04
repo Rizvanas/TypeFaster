ec9e097e1865c58e03a95b17744a4af863f4abf3
Rizvan <rizvanchalilovas@gmail.com>
2019-11-05 01:46:53 +0200

    Add typing accuracy calculation method

---
+++ TypeFaster.GameServices/Contracts/ITypingCalculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using TypeFaster.Domain.Entities;
+
+namespace TypeFaster.GameServices.Contracts
+{
+    public interface ITypingCalculator
+    {
+        int GetNetTypingSpeed(Sentence userInput, DateTime startTime, int totalErrorsMade);
+        int GetGrossTypingSpeed(Sentence userInput, DateTime startTime);
+        decimal GetTypingAccuracy(Sentence userInput, DateTime startTime, int totalErrorsMade);
+    }
+}
---
+++ TypeFaster.GameServices/Implementations/TypingCalculator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using TypeFaster.Common.Contracts;
+using TypeFaster.Domain.Entities;
+using TypeFaster.GameServices.Contracts;
+
+namespace TypeFaster.GameServices.Implementations
+{
+    public class TypingCalculator : ITypingCalculator
+    {
+        private readonly IDateTime _dateTime;
+
+        public TypingCalculator(IDateTime dateTime)
+        {
+            _dateTime = dateTime;
+        }
+
+        public int GetNetTypingSpeed(Sentence userInput, DateTime startTime, int totalErrorsMade)
+        {
+            var wordsTyped = userInput.Words.Count;
+            var elapsedMinutes = (_dateTime.Now - startTime).TotalMinutes;
+
+            var netSpeed = (wordsTyped - totalErrorsMade) / elapsedMinutes;
+            
+            return Convert.ToInt32(Math.Truncate(netSpeed));
+        }
+
+        public int GetGrossTypingSpeed(Sentence userInput, DateTime startTime)
+        {
+            var wordsTyped = userInput.Words.Count;
+            var elapsedMinutes = (_dateTime.Now - startTime).TotalMinutes;
+
+            return Convert.ToInt32(Math.Truncate(wordsTyped / elapsedMinutes));
+        }
+
+        public decimal GetTypingAccuracy(Sentence userInput, DateTime startTime, int totalErrorsMade)
+        {
+            var netTypingSpeed = GetNetTypingSpeed(userInput, startTime, totalErrorsMade);
+            var grossTypingSpeed = GetGrossTypingSpeed(userInput, startTime);
+
+            return (netTypingSpeed / grossTypingSpeed) * 100;
+        }
+    }
+}
