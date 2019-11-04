1058efbf17c84ae09e781397523b0d6350663853
Rizvan <rizvanchalilovas@gmail.com>
2019-11-04 20:52:39 +0200

    Remove Unused dependencies

--- TypeFaster.GameServices/Implementations/SentenceLoader.cs
+++ TypeFaster.GameServices/Implementations/SentenceLoader.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using TypeFaster.Common.Contracts;
 using TypeFaster.Domain.Entities;
 using TypeFaster.GameServices.Contracts;
