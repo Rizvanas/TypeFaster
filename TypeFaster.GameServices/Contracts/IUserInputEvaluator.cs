684c5c96d8120c5000fdd2ee6fbbd82868fc6211
Rizvan <rizvanchalilovas@gmail.com>
2019-11-04 20:47:26 +0200

    Add UserInputEvaluator service class

---
+++ TypeFaster.GameServices/Contracts/IUserInputEvaluator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TypeFaster.Domain.Entities;
+
+namespace TypeFaster.GameServices.Contracts
+{
+    public interface IUserInputEvaluator
+    {
+        KeyValuePair<int, string> GetUserInputError(Sentence userInput, Sentence sentence);
+    }
+}
---
+++ TypeFaster.GameServices/Implementations/UserInputEvaluator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TypeFaster.Domain.Entities;
+using TypeFaster.GameServices.Contracts;
+
+namespace TypeFaster.GameServices.Implementations
+{
+    public class UserInputEvaluator : IUserInputEvaluator
+    {
+        public KeyValuePair<int, string> GetUserInputErrors (Sentence userInput, Sentence sentence)
+        {
+            if (sentence.Words.Last().StartsWith(userInput.Words.Last))
+                
+        }
+    }
+}
