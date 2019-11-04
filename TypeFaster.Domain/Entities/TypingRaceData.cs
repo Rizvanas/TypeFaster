8712926c9cdbe0fdfa802ebaecbd465237ecf7c2
Rizvan <rizvanchalilovas@gmail.com>
2019-11-04 20:41:58 +0200

    Remove TypingRace and Add TypingRaceData instead

--- TypeFaster.Domain/Entities/TypingRaceData.cs
+++ TypeFaster.Domain/Entities/TypingRaceData.cs
@@ -1,17 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
-using TypeFaster.Domain.Contracts;
 
 namespace TypeFaster.Domain.Entities
 {
-    public class ClassicTypingRace : ITypingRace
+    public class TypingRaceData
     {
         public string Title { get; set; }
         public DateTime StartTime { get; set; }
         public DateTime EndTime { get; set; }
         public Sentence Sentence { get; set; }
-        public string UserInput { get; set; }
-        public IList<string> Typos { get; set; } = new List<string>();
+        public Sentence UserInput { get; set; }
+        public int TotalKeyStrokes { get; set; }
+        public IDictionary<int, string> Typos { get; set; }
     }
 }
