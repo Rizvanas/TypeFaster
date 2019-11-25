648925e7e9bc5e05722e279a5775e543958d4e21
Rizvan <rizvanchalilovas@gmail.com>
2019-11-25 22:54:51 +0200

    Remove unused files

--- TypeFaster.GameServices/Contracts/IObserver.cs
+++
@@ -1,9 +0,0 @@
-﻿using TypeFaster.Domain.ValueObjects;
-
-namespace TypeFaster.GameServices.Contracts
-{
-    public interface IObserver
-    {
-        void Update(TypingRaceState typingRaceState);
-    }
-}
