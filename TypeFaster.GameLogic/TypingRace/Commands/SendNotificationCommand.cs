f1904c5c4e962082c97b4ada4dd0f4e88fc1b09e
Rizvan <rizvanchalilovas@gmail.com>
2019-12-15 16:20:27 +0200

    Add tests

---
+++ TypeFaster.GameLogic/TypingRace/Commands/SendNotificationCommand.cs
@@ -0,0 +1,23 @@
+﻿using TypeFaster.GameLogic.Contracts.TypingRace;
+
+namespace TypeFaster.GameLogic.TypingRace.Commands
+{
+    public class SendNotificationCommand : ICommand
+    {
+        private readonly ITypingRaceInstance _typingRaceInstance;
+
+        public SendNotificationCommand(ITypingRaceInstance typingRaceInstance)
+        {
+            _typingRaceInstance = typingRaceInstance;
+        }
+
+        public void Execute()
+        {
+            _typingRaceInstance.Notify();
+        }
+
+        public void Undo()
+        {
+        }
+    }
+}
---
+++ TypeFaster.GameServices.Tests/TypingCalculatorTests.cs
@@ -0,0 +1,128 @@
+﻿using NUnit.Framework;
+using Shouldly;
+using System;
+using TypeFaster.GameServices.Contracts;
+using TypeFaster.GameServices.Implementations;
+
+namespace TypeFaster.GameServices.Tests
+{
+    public class TypingTestData
+    {
+        public string UserInput { get; set; }
+        public TimeSpan ElapsedTime { get; set; }
+        public int TotalErrorsMade { get; set; }
+        public int ExpectedNetSpeed { get; set; }
+        public int ExpectedGrossSpeed { get; set; }
+        public decimal ExpectedAccuracy { get; set; }
+    }
+
+    public class TypingCalculatorTests
+    {
+        private ITypingCalculator _typingCalculator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _typingCalculator = new TypingCalculator();
+        }
+
+        private static TypingTestData[] _typingTestDataSet1 = new[]
+        {
+            new TypingTestData
+            {
+                UserInput = "Labas vakaras, ponai ir ",
+                ElapsedTime = TimeSpan.FromSeconds(60),
+                TotalErrorsMade = 1,
+                ExpectedNetSpeed = 3,
+                ExpectedGrossSpeed = 4,
+                ExpectedAccuracy = 75M
+            },
+            new TypingTestData
+            {
+                UserInput = "Labas vakaras, ponai ir ",
+                ElapsedTime = TimeSpan.FromSeconds(60),
+                TotalErrorsMade = 0,
+                ExpectedNetSpeed = 4,
+                ExpectedGrossSpeed = 4,
+                ExpectedAccuracy = 100M
+            },
+            new TypingTestData
+            {
+                UserInput = "Labas vakaras, ponai ir ",
+                ElapsedTime = TimeSpan.FromSeconds(10),
+                TotalErrorsMade = 0,
+                ExpectedNetSpeed = 24,
+                ExpectedGrossSpeed = 24,
+                ExpectedAccuracy = 100M
+            }
+        };
+        private static TypingTestData[] _typingTestDataSet2 = new[]
+       {
+            new TypingTestData
+            {
+                UserInput = "",
+                ElapsedTime = TimeSpan.FromSeconds(0),
+                TotalErrorsMade = 1,
+                ExpectedNetSpeed = 3,
+                ExpectedGrossSpeed = 4,
+                ExpectedAccuracy = 75M
+            },
+        };
+
+
+        [Test]
+        public void GetNetTypingSpeedReturnsExpectedValue(
+            [ValueSource("_typingTestDataSet1")]TypingTestData testData)
+        {
+            var testResult = _typingCalculator.GetNetTypingSpeed(testData.UserInput, testData.ElapsedTime, testData.TotalErrorsMade);
+
+            testResult.ShouldBe(testData.ExpectedNetSpeed);
+        }
+
+        [Test]
+        public void GetGrossTypingSpeedReturnsExpectedValue(
+            [ValueSource("_typingTestDataSet1")]TypingTestData testData)
+        {
+            var testResult = _typingCalculator.GetGrossTypingSpeed(testData.UserInput, testData.ElapsedTime);
+
+            testResult.ShouldBe(testData.ExpectedGrossSpeed);
+        }
+
+        [Test]
+        public void GetTypingAccuracyReturnsExpectedValue(
+            [ValueSource("_typingTestDataSet1")]TypingTestData testData)
+        {
+            var testResult = _typingCalculator.GetTypingAccuracy(testData.UserInput, testData.TotalErrorsMade);
+
+            testResult.ShouldBe(testData.ExpectedAccuracy);
+        }
+
+        [Test]
+        public void GetNetTypingSpeedThrowsDivideByZeroExceptionWhenElapsedTimeIs0(
+            [ValueSource("_typingTestDataSet2")]TypingTestData testData)
+        {
+            Should.Throw<Exception>(() => _typingCalculator.GetNetTypingSpeed(
+                    testData.UserInput,
+                    testData.ElapsedTime,
+                    testData.TotalErrorsMade));
+        }
+
+        [Test]
+        public void GetGrossTypingSpeedThrowsDivideByZeroExceptionWhenElapsedTimeIs0(
+            [ValueSource("_typingTestDataSet2")]TypingTestData testData)
+        {
+            Should.Throw<Exception>(() => _typingCalculator.GetGrossTypingSpeed(
+                    testData.UserInput,
+                    testData.ElapsedTime));
+        }
+
+        [Test]
+        public void GetTypingAccuracyThrowsDivideByZeroExceptionWhenUserInputLenghtIs0(
+            [ValueSource("_typingTestDataSet2")]TypingTestData testData)
+        {
+            Should.Throw<Exception>(() => _typingCalculator.GetTypingAccuracy(
+                    testData.UserInput,
+                    testData.TotalErrorsMade));
+        }
+    }
+}
