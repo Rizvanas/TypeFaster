99f18cc19c239cba46e23c76616b9ecc552fba20
Rizvan <rizvanchalilovas@gmail.com>
2019-12-16 18:13:10 +0200

    Remove unused imports

--- TypeFaster.Persistence/Contracts/ISentenceRepository.cs
+++ TypeFaster.Persistence/Contracts/ISentenceRepository.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 using TypeFaster.Domain.Entities;
 
 namespace TypeFaster.Persistence.Contracts
--- TypeFaster.UI/Enums/UIPrompt.cs
+++ TypeFaster.UI/Enums/UIPrompt.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿
 namespace TypeFaster.UI.Enums
 {
     public static class UIPrompt
